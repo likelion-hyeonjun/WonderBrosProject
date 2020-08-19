d4f79c400d5c6291a95925ef2800f79591d2208d
HYEONJUN <hyeonjun1882@postech.ac.kr>
2020-08-20 03:00:53 +0900

    finishing fine Tuning Code

--- models.py
+++ models.py
@@ -1,37 +1,37 @@
 import torchvision.models as models
 
-def load(Name, pretrained=False):
-    if Name == 'densenet121':
+def models(name, pretrained=False):
+    if name == 'densenet121':
         model = torchvision.models.densenet121(pretrained=pretrained)
-    elif Name == 'densenet169':
+    elif name == 'densenet169':
         model = torchvision.models.densenet169(pretrained=pretrained)
-    elif Name == 'densenet161':
+    elif name == 'densenet161':
         model = torchvision.models.densenet161(pretrained=pretrained)
-    elif Name == 'densenet201':
+    elif name == 'densenet201':
         model = torchvision.models.densenet201(pretrained=pretrained)
-    elif Name == 'vgg11':
+    elif name == 'vgg11':
         model = torchvision.models.vgg11(pretrained=pretrained)
-    elif Name == 'vgg11_bn':
+    elif name == 'vgg11_bn':
         model = torchvision.models.vgg11_bn(pretrained=pretrained)
-    elif Name == 'vgg13':
+    elif name == 'vgg13':
         model = torchvision.models.vgg13(pretrained=pretrained)
-    elif Name == 'vgg16' :
+    elif name == 'vgg16' :
         model = torchvision.models.vgg16(pretrained=pretrained)
-    elif Name == 'vgg16_bn':
+    elif name == 'vgg16_bn':
         model = torchvision.models.vgg16_bn(pretrained=pretrained)
-    elif Name == 'vgg19':
+    elif name == 'vgg19':
         model = torchvision.models.vgg19(pretrained=pretrained)
-    elif Name == 'vgg19_bn':
+    elif name == 'vgg19_bn':
         model = torchvision.models.vgg19_bn(pretrained=pretrained)
-    elif Name == 'resnet18':
+    elif name == 'resnet18':
         model = torchvision.models.resnet18(pretrained=pretrained)
-    elif Name == 'resnet34' :
+    elif name == 'resnet34' :
         model = torchvision.models.resnet34(pretrained=pretrained)
-    elif Name == 'resnet50' :
+    elif name == 'resnet50' :
         model = torchvision.models.resnet50(pretrained=pretrained)
-    elif Name == 'resnet101':
+    elif name == 'resnet101':
         model = torchvision.models.resnet101(pretrained=pretrained)
-    elif Name == 'resnet152':
+    elif name == 'resnet152':
         model = torchvision.models.resnet152(pretrained=pretrained)
     else :
         model = torchvision.models.vgg19(pretrained=pretrained) #그냥 이거 디폴트함
--- utils.py
+++ utils.py
@@ -1,2 +1,47 @@
-# model finetuning 하는 코드 
-# 
+#함수이름은 lower camel, 변수이름은 under bar lower case
+#파이썬에서? 함수값을 그냥 넣으면 
+from models import models
+import torch.nn as nn
+
+#initialize classification layer?
+
+def freezeResNet(model):
+    for name, p in model.named_parameters():
+        if 'fc' not in name:
+            p.required_grad = False
+    return model #이거 필요한가?
+
+def freezeDenseNet(model):
+    for name, p in model.named_parameters():
+        if 'classifier' not in name:
+            p.required_grad = False
+    return model
+
+def freezeVGG(model):
+    for name, p in model.named_parameters():
+        if 'classifier.6' not in name:
+            p.required_grad = False
+    return model
+
+
+def fineTuningModel(name, num_classes, is_freeze=True, pretrained=False): #freeze
+
+    model = models(name, pretrained) 
+    if 'resnet' in name:
+        input_features = model.fc.in_features
+        model.fc = nn.Linear(in_features = input_features, out_features = num_classes, bias=True)
+        if is_freeze and pretrained :
+            model = freezeResNet(model)
+    elif 'vgg' in name:
+        input_features = model.classifier[-1].in_features
+        model.classifier[-1] = nn.Linear(in_features = input_features, out_features = num_classes, bias=True)
+        if is_freeze and pretrained:
+            model = freezeVGG(model)
+    elif 'densenet' in name:
+        input_features = model.classifier.in_features
+        model.classifier = nn.Linear(in_features = input_features, out_features = num_classes, bias=True)
+        if is_freeze and pretrained:
+            model = freezeDenseNet(model)
+    
+    return model
+    
